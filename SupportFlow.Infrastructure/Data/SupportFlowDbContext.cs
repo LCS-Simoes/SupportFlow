b07d0c74d20eab2c603b6263e22f8ddfce3d1051
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-12 18:55:00 -0300

    chore: config db context

---
+++ SupportFlow.Infrastructure/Data/SupportFlowDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupportFlow.Domain.Entities;
+using SupportFlow.Infrastructure.Data.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportFlow.Infrastructure.Data
+{
+    public class SupportFlowDbContext : DbContext
+    {
+        public SupportFlowDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Suporte> Suportes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new SuporteMap());
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected SupportFlowDbContext()
+        {
+        }
+    }
+}
