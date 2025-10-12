55c69a9f55b975a1f5ae6c5ebcb323d736e10d46
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-12 18:53:55 -0300

    feat: Repositorios Usuarios e Tarefas

--- SupportFlow.Infrastructure/Data/Repositories/SuporteRepository.cs
+++ SupportFlow.Infrastructure/Data/Repositories/SuporteRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SupportFlow.Domain.Entities;
+using SupportFlow.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,66 @@ using System.Threading.Tasks;
 
 namespace SupportFlow.Infrastructure.Data.Repositories
 {
-    internal class SuporteRepository
+    public class SuporteRepository : ISuporte
     {
+        private readonly SupportFlowDbContext _dbcontext;
+        public SuporteRepository(SupportFlowDbContext supportFlowDbContext) 
+        { 
+            _dbcontext = supportFlowDbContext;
+        }
+
+        public async Task<List<Suporte>> TodosChamados()
+        {
+            return await _dbcontext.Suportes
+                .Include(x => x.Usuario)
+                .ToListAsync();
+        }
+
+        public async Task<Suporte> BuscarID(int id)
+        {
+            return await _dbcontext.Suportes
+                .Include(x => x.Usuario)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+       
+        public async Task<Suporte> Criar(Suporte suporte)
+        {
+            await _dbcontext.Suportes.AddAsync(suporte);
+            await _dbcontext.SaveChangesAsync();
+            return suporte;
+        }
+
+        public async Task<Suporte> Atualizar(Suporte suporte, int id)
+        {
+            Suporte suporteID = await BuscarID(id);
+
+            if (suporteID == null)
+            {
+                throw new Exception($"Tarefa ID: {id} não foi encontrado no banco de dados");
+            }
+
+            suporteID.Nome = suporte.Nome;
+            suporteID.Descricao = suporte.Descricao;
+            suporteID.Status = suporte.Status;
+            suporteID.UsuarioID = suporte.UsuarioID;
+
+            _dbcontext.Suportes.Update(suporteID);
+            await _dbcontext.SaveChangesAsync();
+            return suporteID;
+        }
+
+        public async Task<bool> Deletar(int id)
+        {
+            Suporte suporteID = await BuscarID(id);
+
+            if (suporteID == null)
+            {
+                throw new Exception($"Suporte: {id} não foi encontrado no banco de dados");
+            }
+
+            _dbcontext.Suportes.Remove(suporteID);
+            await _dbcontext.SaveChangesAsync();
+            return true;
+        }  
     }
 }
--- SupportFlow.Infrastructure/Data/Repositories/UsuarioRepository.cs
+++ SupportFlow.Infrastructure/Data/Repositories/UsuarioRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SupportFlow.Domain.Entities;
+using SupportFlow.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,68 @@ using System.Threading.Tasks;
 
 namespace SupportFlow.Infrastructure.Data.Repositories
 {
-    internal class SuporteRepository
+    public class UsuarioRepository : IUsuario
     {
+        private readonly SupportFlowDbContext _dbcontext;
+        public UsuarioRepository(SupportFlowDbContext supportFlowDbContext)
+        {
+            _dbcontext = supportFlowDbContext;
+        }
+
+        public async Task<List<Usuario>> TodosUsuarios()
+        {
+            //Fazer depois
+            return await _dbcontext.Usuarios.ToListAsync();
+        }
+
+        public async Task<Usuario> BuscarId(int id)
+        {
+            return await _dbcontext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Task<Usuario> BuscarLogin(string Login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Usuario> Cadastrar(Usuario usuario)
+        {
+            await _dbcontext.Usuarios.AddAsync(usuario);
+            await _dbcontext.SaveChangesAsync();
+            return usuario;
+        }
+
+        public async Task<Usuario> Atualizar(Usuario usuario, int id)
+        {
+            Usuario usuarioId = await BuscarId(id);
+            if(usuarioId == null)
+            {
+                throw new Exception($"Usuário {id} não foi encontrado no banco de dados");
+            }
+
+            usuarioId.Nome = usuario.Nome;
+            usuarioId.Username = usuario.Username;
+            usuarioId.Setor = usuario.Setor;
+            usuarioId.Perfil = usuario.Perfil;
+
+            _dbcontext.Usuarios.Update(usuarioId);
+            await _dbcontext.SaveChangesAsync();
+
+            return usuarioId;
+        }
+
+        public async Task<bool> Deletar(int id)
+        {
+            Usuario usuarioID = await BuscarId(id);
+
+            if (usuarioID == null)
+            {
+                throw new Exception($"Usuário {id} não foi encontrado no banco de dados");
+            }
+
+            _dbcontext.Usuarios.Remove(usuarioID);
+            await _dbcontext.SaveChangesAsync();
+            return true;
+        }
     }
 }
