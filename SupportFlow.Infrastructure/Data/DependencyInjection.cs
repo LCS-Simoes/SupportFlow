10bb7cd954d4371f593ab8dfffa5f6563c9ab38d
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-18 00:05:14 -0300

    feat: connect db na api

---
+++ SupportFlow.Infrastructure/Data/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SupportFlow.Domain.Interfaces;
+using SupportFlow.Infrastructure.Data.Repositories;
+
+namespace SupportFlow.Infrastructure.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<SupportFlowDbContext>(options =>
+                options.UseSqlite(connectionString));
+
+            services.AddScoped<IUsuario, UsuarioRepository>();
+            services.AddScoped<ISuporte, SuporteRepository>();
+
+            return services;
+        } 
+    }
+}
