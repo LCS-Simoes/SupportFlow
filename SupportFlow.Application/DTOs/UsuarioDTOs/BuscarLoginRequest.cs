3e577aa17ccbf8014f909d40fa4f0732d022b69f
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-22 20:06:02 -0300

    feat(Usuarios): DTO de buscar informações

---
+++ SupportFlow.Application/DTOs/UsuarioDTOs/BuscarLoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportFlow.Application.DTOs.UsuarioDTOs
+{
+    public class BuscarLoginRequest
+    {
+        public string Username { get; set; }
+        public string Senha {  get; set; }
+        public bool LembrarMe { get; set; }
+    }
+}
