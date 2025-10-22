6c7da0260a7b9860f87f20d5ede2dae5a43dcbda
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-22 20:09:13 -0300

    feat(Usuarios): Método para fazer as senhas em hash

---
+++ SupportFlow.Application/Utils/SenhaHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportFlow.Application.Utils
+{
+    public static class SenhaHelper
+    {
+        public static string GerarHash(string senha)
+        {
+            using var sha = SHA256.Create();
+            var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(senha));
+            return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+        }
+
+        public static bool VerificarSenha(string senhaDigitada, string hashSalvo)
+        {
+            var hashDigitado = GerarHash(senhaDigitada);
+            return hashDigitado == hashSalvo;
+        }
+    }
+}
