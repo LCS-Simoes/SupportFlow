013358712fe21fa16056759f8490f3e543e4bdca
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-24 21:27:57 -0300

    refactor: Melhorando o código e adicionando funcionalidade

--- SupportFlow.Application/UseCases/Suportes/CriarSuporteUseCase.cs
+++ SupportFlow.Application/UseCases/Suportes/CriarSuporteUseCase.cs
@@ -13,21 +13,27 @@ namespace SupportFlow.Application.UseCases.Suportes
     public class CriarSuporteUseCase
     {
         public readonly ISuporte _suporteRepository;
+        public readonly IUsuario _usuarioRepository;
 
-        public CriarSuporteUseCase(ISuporte suporteRepository)
+        public CriarSuporteUseCase(ISuporte suporteRepository, IUsuario usuarioRepository)
         {
             _suporteRepository = suporteRepository;
+            _usuarioRepository = usuarioRepository;
         }
 
         public async Task<SuporteResponse> ExecuteAsync(CriarSuporteResquest request)
         {
+            var usuario = await _usuarioRepository.BuscarId(request.UsuarioID);
+            if (usuario == null) throw new Exception("Usuário não encontrado");
+
             var suporte = new Suporte
             {
                 Nome = request.Nome,
                 Descricao = request.Descricao,
-                UsuarioID = request.UsuarioID,
                 Status = Status.Aberta,
-                DataSuporte = DateTimeOffset.UtcNow
+                DataSuporte = DateTimeOffset.UtcNow,
+                UsuarioID = request.UsuarioID,
+                Usuario = usuario
             };
 
             var criado = await _suporteRepository.Criar(suporte);
@@ -38,7 +44,9 @@ namespace SupportFlow.Application.UseCases.Suportes
                 Nome = criado.Nome!,
                 Descricao = criado.Descricao!,
                 Status = criado.Status,
-                DataSuporte = criado.DataSuporte
+                DataSuporte = criado.DataSuporte,
+                UsuarioID= usuario.Id,
+                NomeUsuario = usuario.Nome
             };
         }
     }
