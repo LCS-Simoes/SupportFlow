0b80ce9183e95cd393ae2ba361e4d3478423c0c1
Lucas Simões <lucasbritosimoes@hotmail.com>
2025-10-24 21:24:57 -0300

    feat(Usuario): Criação de login mais Token e Suportes exclusivos

--- SupportFlow.API/Controllers/AuthController.cs
+++ SupportFlow.API/Controllers/AuthController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using SupportFlow.Application.DTOs.SuporteDTOs;
 using SupportFlow.Application.DTOs.UsuarioDTOs;
+using SupportFlow.Application.UseCases.Suportes;
 using SupportFlow.Application.UseCases.Usuarios;
+using SupportFlow.Domain.Entities;
 using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -15,12 +19,14 @@ namespace SupportFlow.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly AutenticacaoUseCase _autenticarUsuario;
+        private readonly TicketUsuarioUseCase _ticket;
         private readonly IConfiguration _config;
 
-        public AuthController(AutenticacaoUseCase autenticacao, IConfiguration config)
+        public AuthController(AutenticacaoUseCase autenticacao, IConfiguration config, TicketUsuarioUseCase ticketUser)
         {
             _autenticarUsuario = autenticacao;
             _config = config;
+            _ticket = ticketUser;
         }
 
         [HttpPost("login")]
@@ -34,7 +40,7 @@ namespace SupportFlow.API.Controllers
 
             var expiracao = request.LembrarMe ? DateTime.UtcNow.AddDays(7) : DateTime.UtcNow.AddHours(2);
 
-            var token = GerarJwtToken(usuario.Username, usuario.Perfil.ToString(), expiracao);
+            var token = GerarJwtToken(usuario, usuario.Username, usuario.Perfil.ToString(), expiracao);
 
             return Ok(new
             {
@@ -52,8 +58,28 @@ namespace SupportFlow.API.Controllers
 
         }
 
+        [Authorize]
+        [HttpGet("tickets")]
+        public async Task<ActionResult<List<SuporteResponse>>> TicketUsuario()
+        {
+            var userIdClaim = User.FindFirst("id");
+            if (userIdClaim == null || !int.TryParse(userIdClaim.Value, out int usuarioId))
+            {
+                return Unauthorized("ID de usuário não encontrado ou inválido no token.");
+            }
+            try
+            { 
+                var result = await _ticket.Handle(usuarioId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Ocorreu um erro interno ao processar sua solicitação de tickets.");
+            }
+        }
+
         //Adicionar no local correto posteriormente
-        private string GerarJwtToken(string username, string perfil, DateTime expiracao)
+        private string GerarJwtToken(Usuario usuario, string username, string perfil, DateTime expiracao)
         {
             var key = Environment.GetEnvironmentVariable("JWT_KEY") ?? _config["Jwt:Key"];
             var issuer = Environment.GetEnvironmentVariable("JWT_ISSUER") ?? _config["Jwt:Issuer"];
@@ -64,6 +90,7 @@ namespace SupportFlow.API.Controllers
 
             var claims = new[]
             {
+                 new Claim("id", usuario.Id.ToString()),
                  new Claim(ClaimTypes.Name, username),
                  new Claim(ClaimTypes.Role, perfil)
             };
